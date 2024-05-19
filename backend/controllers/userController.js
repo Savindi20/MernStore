d1e86f027b274f8c55b140b75748227aaca99130
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2024-05-19 15:19:02 +0530

    asynchandler added and update project

---
+++ backend/controllers/userController.js
@@ -0,0 +1,9 @@
+import User from "../models/userModel.js";
+import asyncHandler from "../middlewares/asyncHandler.js";
+
+const createUser = asyncHandler(async (req, res) => {
+    res.send("Create User");
+});
+
+
+export {createUser};
--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,14 @@
-//pachages
-import path from 'path';
-import express from 'express';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
+// packages
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
-//uliles
-import connectDB from './config/db.js';
+// Utiles
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
 
-dotenv.config()
+dotenv.config();
 const port = process.env.PORT || 5000;
 
 connectDB();
@@ -16,8 +17,8 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());  
+app.use(cookieParser());
 
-app.use('/api/users', userRoutes);
+app.use("/api/users", userRoutes);
 
-app.listen(port, () => console.log(`Server running on port ${port}`))
+app.listen(port, () => console.log(`Server running on port: ${port}`));
