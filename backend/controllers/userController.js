aa9278403afb42139119d39a0339409c8bcf278e
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2024-05-19 21:13:11 +0530

    user controller create user save added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,37 @@
 import User from "../models/userModel.js";
 import asyncHandler from "../middlewares/asyncHandler.js";
+import bcrypt from "bcryptjs";
 
 const createUser = asyncHandler(async (req, res) => {
-    res.send("Create User");
+    const { username, email, password } = req.body;
+    // console.log(username);
+    // console.log(email);
+    // console.log(password);
+
+    if (!username || !email || !password) {
+        res.status(400);
+        throw new Error("Please fill in all fields");
+    }
+
+    const userExists = await User.findOne({ email});
+    if (userExists) {
+        res.status(400).send("User already exists");
+    }
+
+    const newUser = new User({username,email,password});
+
+    try {
+        await newUser.save();
+        res.status(201).json({
+            _id:newUser._id,
+            username:newUser.username,
+            email:newUser.email,
+            isAdmin:newUser.isAdmin,
+        });
+    } catch (error) {
+        res.status(400)
+        throw new Error("Invalid user data");
+    }
 });
 
 
