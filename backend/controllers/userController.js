afed526e868a289754473c408624b7caed49b5cd
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2024-05-20 09:14:23 +0530

    create token added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 import User from "../models/userModel.js";
 import asyncHandler from "../middlewares/asyncHandler.js";
 import bcrypt from "bcryptjs";
+import createToken from "../utils/createToken.js";
 
 const createUser = asyncHandler(async (req, res) => {
     const { username, email, password } = req.body;
@@ -25,6 +26,8 @@ const createUser = asyncHandler(async (req, res) => {
 
     try {
         await newUser.save();
+        createToken(res, newUser._id);
+
         res.status(201).json({
             _id:newUser._id,
             username:newUser.username,
@@ -37,5 +40,9 @@ const createUser = asyncHandler(async (req, res) => {
     }
 });
 
+const loginUser = asyncHandler(async (req, res) => {
 
-export {createUser};
+});
+
+
+export {createUser, loginUser};
