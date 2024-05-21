ea2343ddb03e1dd61c3939b35b770642a5b8c47a
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2024-05-21 10:38:50 +0530

    updateCurrentUserProfile

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,7 +4,8 @@ import {
     loginUser,
     logoutCurrentUser,
     getAllUsers,
-    getCurrentUserProfile
+    getCurrentUserProfile,
+    updateCurrentUserProfile,
 } from "../controllers/userController.js";
 
 import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
@@ -16,6 +17,6 @@ router.route("/").post(createUser).get(authenticate, authorizeAdmin, getAllUsers
 router.post("/auth", loginUser);
 router.post("/logout", logoutCurrentUser);
 
-router.route("/profile").get(authenticate, getCurrentUserProfile);
+router.route("/profile").get(authenticate, getCurrentUserProfile).put(authenticate, updateCurrentUserProfile);
 
 export default router;
