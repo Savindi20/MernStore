215caa5f938602268631623d9dcc1ba3d5ab3845
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2024-05-29 16:22:04 +0530

    navigation update with toggleDropdown

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,8 @@ import App from './App.jsx'
 import './index.css'  // Import the compiled Tailwind CSS
 import { Route, RouterProvider, createRoutesFromElements } from 'react-router'
 import { createBrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './redux/store.js'
 
 const routes = createBrowserRouter(
   createRoutesFromElements(
@@ -12,5 +14,7 @@ const routes = createBrowserRouter(
 )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={routes} />
+  <Provider store={store}>  // Wrap the RouterProvider in a Provider
+    <RouterProvider router={routes} />
+  </Provider>
 )
