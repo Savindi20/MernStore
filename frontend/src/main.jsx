7b0101b85f247c0aab58643996783de089a6aec2
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2024-06-07 13:52:03 +0530

    Product form fronntend added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,6 @@ import store from "./redux/store";
 import { Route, RouterProvider, createRoutesFromElements } from "react-router";
 import { createBrowserRouter } from "react-router-dom";
 import PrivateRoute from "./components/PrivateRoute";
-
 // Auth
 import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
@@ -15,25 +14,30 @@ import Profile from "./pages/User/Profile";
 import UserList from "./pages/Admin/UserList";
 import CategoryList from "./pages/Admin/CategoryList";
 
+import ProductList from "./pages/Admin/ProductList";
+import AllProducts from "./pages/Admin/AllProducts";
+import ProductUpdate from "./pages/Admin/ProductUpdate";
+
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-
       {/* Registered users */}
       <Route path="" element={<PrivateRoute />}>
         <Route path="/profile" element={<Profile />} />
       </Route>
-
       <Route path="/admin" element={<AdminRoute />}>
         <Route path="userlist" element={<UserList />} />
         <Route path="categorylist" element={<CategoryList />} />
+        <Route path="productlist" element={<ProductList />} />
+        <Route path="allproductslist" element={<AllProducts />} />
+        <Route path="productlist/:pageNumber" element={<ProductList />} />
+        <Route path="product/update/:_id" element={<ProductUpdate />} />
       </Route>
     </Route>
   )
 );
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <RouterProvider router={router} />
